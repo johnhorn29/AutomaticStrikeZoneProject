d2f9b2a52fa62376601eaebf651ac3e12e4adaf9
John Horn <john.horn29@student.tamdistrict.org>
2026-09-02 16:53:39 -0700

    first project

---
+++ Day2/test1.py
@@ -0,0 +1,11 @@
+print("sd")
+Age = int(input("what is your age?"))
+
+print(Age)
+if Age >= 21:
+    print("you can buy alchohol")
+elif Age < 0:
+    print(" You are not human")
+else:
+    print("you can't buy alchohol")
+
