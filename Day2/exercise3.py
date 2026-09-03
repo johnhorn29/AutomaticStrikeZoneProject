1e71f56e9777e38a1ed6b7d723bfa7c76387656d
John Horn <john.horn29@student.tamdistrict.org>
2026-09-02 17:30:05 -0700

    Excercise 3

---
+++ Day2/exercise3.py
@@ -0,0 +1,9 @@
+list = [1, 1, 2, 3, 5, 8, 13, 21, 34]
+list_under_10=[]
+under_what = int(input("What number? "))
+
+for i in list:
+    
+    if i<under_what:
+        list_under_10.append(i)
+print(list_under_10)
