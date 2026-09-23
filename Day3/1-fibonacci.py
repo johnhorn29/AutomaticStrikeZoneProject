10bc812e886349a885a8c2f7b44f0edc01e94c11
John Horn <john.horn29@student.tamdistrict.org>
2026-09-23 16:36:15 -0700

    Function practice

--- Day3/1-fibonacci.py
+++ Day3/1-fibonacci.py
@@ -1,2 +1,15 @@
 # Use functions to print out the fibonacci numbers
-# 1, 1, 2, 3, 5, 7, ...
+# 1, 1, 2, 3, 5, 8, ...
+def fc (f):
+    celcius = (f-32)/(9/5)
+
+    return celcius
+
+print(fc(32))
+
+def cf (c):
+    farenheight = (9/5)*(c)+32
+
+    return farenheight
+
+print(cf(0))
