31e8af9c18285aba4eec5f4e41434746701b74d7
Dillon Wilson <greasyRooster1@gmail.com>
2026-09-23 17:28:03 -0700

    animation example

--- Day3/2-graphics.py
+++ Day3/2-graphics.py
@@ -2,14 +2,20 @@
 
 from lib import *
 
+
+x = 0
+y = 0
+
 def setup():
     pass
 
 def draw():
+    global x,y
     background(220,36,74)
     fill(20,77,145)
     stroke(0,255,255)
-    rect(2,227,24,59)
+    rect(x,227,24,59)
+    x+=1
     
 
 
